d53f1a55f48aa874603163cb096558c65ccd0db1
cc0a <jmckeeco84@gmail.com>
2020-03-11 22:27:15 -0600

    Inheritance + Subclasses

--- oop.py
+++ oop.py
@@ -20,27 +20,59 @@ class Employee:
         return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amount)
+        self.pay = int(self.pay * self.raise_amt)
 
-    @classmethod # Class method will apply to entire class, not just one instance
-    def set_raise_amt(cls, amount):
-        cls.raise_amt = amount
+    # @classmethod # Class method will apply to entire class, not just one instance
+    # def set_raise_amt(cls, amount):
+    #     cls.raise_amt = amount
+    #
+    # @classmethod
+    # def from_string(cls, emp_str):
+    #     first, last, pay = emp_str.split('-')
+    #     return cls(first, last, pay)
+    #
+    # @staticmethod # Don't take instance or class as first arg, a good indicator that a method should be static is if you don't access the instance/class within the function
+    # def is_workday(day):
+    #     if day.weekday() == 5 or day.weekday() == 6:
+    #         return False
+    #     return True
 
-    @classmethod
-    def from_string(cls, emp_str):
-        first, last, pay = emp_str.split('-')
-        return cls(first, last, pay)
+class Developer(Employee): # Inheriting from employee class
+    raise_amt = 1.10
 
-    @staticmethod # Don't take instance or class as first arg, a good indicator that a method should be static is if you don't access the instance/class within the function
-    def is_workday(day):
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay) # super().__init__ brings in the attributes from employee class
+        self.prog_lang = prog_lang
 
-emp_1 = Employee('Corey', 'Schaeffer', 50000)
-emp_2 = Employee('Test', 'User', 60000)
+class Manager(Employee):
 
-import datetime
-my_date = datetime.date(2020, 3, 10)
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
 
-print(Employee.is_workday(my_date))
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+
+dev_1 = Developer('Corey', 'Schaeffer', 50000, 'Python')
+dev_2 = Developer('Test', 'Employee', 60000, 'Java')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+print(issubclass(Manager, Developer))
+print(issubclass(Developer, Employee))
+print(isinstance(mgr_1, Manager))
+print(isinstance(mgr_1, Developer))
