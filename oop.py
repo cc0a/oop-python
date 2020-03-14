4b65a3d816f1599371e15a8c3f22f5bd58f1df10
cc0a <jmckeeco84@gmail.com>
2020-03-13 18:34:43 -0600

    getters, setters, and deleters

--- oop.py
+++ oop.py
@@ -2,37 +2,56 @@ class Employee:
 
     raise_amt = 1.04
 
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.email = first + '.' + last + '@email.com'
-        self.pay = pay
 
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amt)
+    # def apply_raise(self):
+    #     self.pay = int(self.pay * self.raise_amt)
+    #
+    # def __repr__(self):
+    #     return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+    #
+    # def __str__(self):
+    #     return '{} - {}'.format(self.fullname(), self.email)
+    #
+    # def __add__(self, other):
+    #     return self.pay + other.pay
+    #
+    # def __len__(self):
+    #     return len(self.fullname())
 
-    def __repr__(self):
-        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
 
-    def __str__(self):
-        return '{} - {}'.format(self.fullname(), self.email)
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
 
-    def __add__(self, other):
-        return self.pay + other.pay
+emp_1 = Employee('John', 'Smith')
 
-    def __len__(self):
-        return len(self.fullname())
+emp_1.fullname = 'Corey Schaeffer'
 
-
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test', 'Employee', 60000)
+# emp_2 = Employee('Test', 'Employee', 60000)
 
 # print(emp_1 + emp_2)
 
-print(emp_1)
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
 
-print(repr(emp_1))
-print(str(emp_1))
+del emp_1.fullname
+# print(emp_1.fullname())
